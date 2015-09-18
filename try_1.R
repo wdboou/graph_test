a6082974b51c428a5b2b89b671fe1bbd0324e75f
wdboou <wdboou@gmail.com>
2015-09-18 10:53:21 -0700

    minor change

--- try_1.R
+++ try_1.R
@@ -10,7 +10,10 @@ layout <- layout.fruchterman.reingold
 
 # plot 1
 dol <- graph.data.frame(dolphins)
-jpeg(filename='dolphins.jpg', width=800, height=800, units='px')
+jpeg(filename='dolphins.jpg',
+     width=800, 
+     height=800, 
+     units='px')
 plot(dol,
      vertex.size=5,
      layout=layout,
@@ -21,21 +24,33 @@ plot(dol,
 dev.off()
 
 # plot 2
-jpeg(filename="dolphins_sub1.jpg",width=800, height=800,units='px')
-gn <- graph.neighborhood(dol, order=1)
+jpeg(filename="dolphins_sub1.jpg",
+     width=800, 
+     height=800,
+     units='px')
+gn <- graph.neighborhood(dol, 
+                         order=1)
 plot(gn[[1]],
      layout=layout)
 dev.off()
 
 # plot 3
-jpeg(filename="dolphins_sub2.jpg",width=800, height=800,units='px')
-gn <- graph.neighborhood(dol, order=2)
+jpeg(filename="dolphins_sub2.jpg",
+     width=800, 
+     height=800,
+     units='px')
+gn <- graph.neighborhood(dol, 
+                         order=2)
 plot(gn[[2]],
      layout=layout)
 dev.off()
 # plot 4
-jpeg(filename="dolphins_sub3.jpg",width=800, height=800,units='px')
-gn <- graph.neighborhood(dol, order=1)
+jpeg(filename="dolphins_sub3.jpg",
+     width=800, 
+     height=800,
+     units='px')
+gn <- graph.neighborhood(dol, 
+                         order=1)
 plot(gn[[1]]+gn[[2]],
      layout=layout)
 dev.off()
@@ -43,47 +58,96 @@ dev.off()
 # plot 5
 source("http://michael.hahsler.net/SMU/ScientificCompR/code/map.R")
 E(dol)$curved <- 0.2   # #?????????????????????,????????????????????????
-jpeg(filename='dolphins_curve1.jpg',width=800,height=800,units='px')
+jpeg(filename='dolphins_curve1.jpg',
+     width=800,
+     height=800,
+     units='px')
 plot(dol, 
      layout=layout,
-     vertex.size = map(degree(dol),c(1,20)),
-     vertex.color= map(degree(dol),c(1,20)))
+     vertex.size = map(degree(dol),
+                       c(1,20)),
+     vertex.color= map(degree(dol),
+                       c(1,20)))
 dev.off()
 
 # plot 6
 cl <- optimal.community(dol)
 E(dol)$curved <- 0
-jpeg(filename="dolphins_commu2.jpg", width=800, height=800, units='px')
+jpeg(filename="dolphins_commu2.jpg", 
+     width=800, 
+     height=800, 
+     units='px')
 plot(dol,
      layout=layout,
-     vertex.size = map(degree(dol),c(1,20)),
+     vertex.size = map(degree(dol),
+                       c(1,20)),
      vertex.color = rainbow(10,.8,.8,alpha=.8)[cl$membership+1L])
 dev.off()
 
 # plot 7
-sg1 <- cluster_spinglass(dol, spins = 3, gamma=1.0) # spin is the number of cummunity
-jpeg(filename="dolphins_commu3.jpg", width=800, height=800,units='px')
+sg1 <- cluster_spinglass(dol, 
+                         spins = 3, 
+                         gamma=1.0) # spin is the number of cummunity
+jpeg(filename="dolphins_commu3.jpg", 
+     width=800, 
+     height=800,
+     units='px')
 plot(dol,
      layout=layout,
      vertex.size=5,
-     vertex.color=rainbow(10,.8,.8,alpha=0.8)[sg1$membership])
+     vertex.color=rainbow(10,
+                          .8,
+                          .8,
+                          alpha=0.8)[sg1$membership])
 dev.off()
 
 # plot 8
 sg1 <- cluster_spinglass(dol, spins=3, gamma=1.0)
-jpeg(filename='dolphins_subcommu.jpg', width=800, height=800, units='px')
-subg <- induced.subgraph(dol, which(membership(sg1)==1))
-plot(subg, layout=layout, vertex.size=5, vertex.color=1,)
+jpeg(filename='dolphins_subcommu.jpg', 
+     width=800, 
+     height=800, 
+     units='px')
+subg <- induced.subgraph(dol, 
+                         which(membership(sg1)==1))
+plot(subg, 
+     layout=layout, 
+     vertex.size=5, 
+     vertex.color=1,)
 dev.off()
 
 # plot 9
-sg1 <- cluster_spinglass(dol, spins=3, gamma=1.0)
-jpeg(filename='dolphins_commu10.jpg',width=800, height=800, units='px')
-layout.circle
-plot()
+sg1 <- cluster_spinglass(dol, 
+                         spins=3, 
+                         gamma=1.0)
+jpeg(filename='dolphins_commu10.jpg',
+     width=800, 
+     height=800,
+     units='px')
+plot(dol,layout=layout.circle,
+     vertex.size=5, 
+     vertex.color=rainbow(10,
+                          .8,
+                          .8,
+                          alpha=.8)[sg1$membership])
+dev.off()
 
 # plot 10
-
+sg1 <- cluster_spinglass(dol, 
+                         spins=3, 
+                         gamma=1.0)
+jpeg(filename='dolphins_commu11.jpg',
+     width=800,
+     height=800,
+     units='px')
+layout = layout.sphere
+plot(dol, 
+     layout=layout,
+     vertex.size=5,
+     vertex.color=rainbow(10,
+                          .8,
+                          .8,
+                          alpha=.8)[sg1$membership])
+dev.off()
 
 # ok finish this part
 
